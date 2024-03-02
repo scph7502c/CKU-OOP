b1acdd0f8fe3e26be60bf9535a37c1c069edabd1
Damian <damian.temimi@gmail.com>
2024-03-02 10:23:33 +0100

    wip

---
+++ pole.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"Wprowadz pierwsza liczbe: ";
+	cin>>a;
+	cout<<endl;
+	cout<<"Wprowadz druga liczbe: ";
+	cin>>b;
+	cout<<endl;
+	cout<<"Pole prostokata wynosi: ";
+	int poleProstokata=a*b;
+	cout<<poleProstokata;
+	return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+  int liczba1;
+  cin>>liczba1;
+  int liczba2 = liczba1+2;
+  cout<<liczba2;
+  
+  return 0;
+
+}
