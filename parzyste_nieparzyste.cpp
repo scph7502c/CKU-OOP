59999bc07e53c2dc4dff262131601c4bd5674541
Damian <damian.temimi@gmail.com>
2024-03-02 13:32:04 +0100

    wip

---
+++ parzyste_nieparzyste.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int liczba;
+	cout<<"Podaj liczbe :"<<endl;
+	cin>>liczba;
+	if(liczba%2==0){
+		cout<<"Liczba jest parzysta";
+	} else {
+		cout<<"Liczba jest nieparzysta";
+	}
+	return 0;
+		}
+		
+
