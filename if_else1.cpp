f3cf1db4764756a010e0c7d1a8893bcbd2d52312
Damian <damian.temimi@gmail.com>
2024-03-02 11:40:31 +0100

    wip

--- if_else1.cpp
+++ if_else1.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main()
 {
 	int x, w;
-	cout<<"Podaj liczbê x: ";
+	cout<<"Podaj liczbe x: ";
 	cin>>x;
-	if(x=0) {
+	if(x>=0) {
 		w=x;
-		cout<<"Wartoœæ bezwzglêdna liczby x="<<w;
+		cout<<"Wartosc bezwzgledna liczby x: "<<w;
 	}
 	else {
 		w=-x;
-		cout<<"Wartoœæ bezwzglêdna liczby x="<<w;
+		cout<<"Wartosc bezwzgledna liczby x: "<<w;
 	}
 	return 0;
 }
---
+++ liczby.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cout<<"Wprowadz liczbe: ";
+	cin>>a;
+	if(a>0){
+		cout<<"Twoja liczba jest dodatnia";
+	} else {
+		cout<<"Twoja liczba jest niedodatnia";
+	}
+}
+
