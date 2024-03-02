4907e5551c74da77a2cd7c45f23206e63889fe6f
Damian <damian.temimi@gmail.com>
2024-03-02 10:46:21 +0100

    wip

---
+++ trojkat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a, h, pole;
+cout<<"podaj a: ";
+cin>>a;
+cout<<endl;
+cout<<"Podaj h: ";
+cin>>h;
+cout<<endl;
+pole=a*h/2;
+cout<<"Pole trojkata wynosi: ";
+cout<<pole;
+return 0;
+}
